1e067a2306d8ce5a5dec29bffddb2cc2398ffdec
starihal <starihal@STARIHAL-LAP.in.oracle.com>
2019-04-16 12:51:28 +0530

    test conflict

--- Application1/src/com/ofss/Calculator.java
+++ Application1/src/com/ofss/Calculator.java
@@ -2,9 +2,7 @@ package com.ofss;
 
 public class Calculator {
 
-	public int add(int a, int b) {
-		return a+b;
-		
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
